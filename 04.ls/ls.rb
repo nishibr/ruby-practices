8ea2ee0f2f77dab6343450b15648bb1520eba2ca
nishibr <nishibr004@gmail.com>
2023-10-22 14:21:27 +0900

    Rv指摘修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,21 +1,27 @@
 # frozen_string_literal: true
 
-NUMBER_OF_COL = 3
-target_dir = '*'
+COLUMN_LENGTH = 3
+FILE_SPACE = 2
+TARGET_DIR = '*'
 
-def get_file_list(target_dir)
-  Dir.glob(target_dir)
+def get_files
+  Dir.glob(TARGET_DIR)
 end
 
-def output(file_list)
-  number_of_row = (file_list.size / NUMBER_OF_COL.to_f).ceil
+def get_max_file_space(files)
+  files.map {|file| file.size}.max + FILE_SPACE
+end
+
+def output(files)
+  max_file_space = get_max_file_space(files)
+  number_of_row = (files.size / COLUMN_LENGTH.to_f).ceil
   number_of_row.times do |i|
-    NUMBER_OF_COL.times do |j|
-      print file_list[number_of_row * j + i].ljust(11) if !file_list[number_of_row * j + i].nil?
+    COLUMN_LENGTH.times do |j|
+      print files[number_of_row * j + i].ljust(max_file_space) if !files[number_of_row * j + i].nil?
     end
     puts
   end
 end
 
-file_list = get_file_list(target_dir)
-output(file_list)
+files = get_files
+output(files)
